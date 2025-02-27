73e5b5516b378d926058feceaf24482c8f82996a
bkbarath <barathcoder5@gmail.com>
2025-02-27 09:15:06 +0530

    [TOPIC] Operators

---
+++ src/operators/AssignmentOperator.java
@@ -0,0 +1,15 @@
+package operators;
+
+public class AssignmentOperator {
+
+    public static void main(String[] args) {
+
+        int a =10, b=5, c=15;
+
+        short as = 10;
+        short bs = 10;
+
+        as += bs;
+        System.out.println(as);
+    }
+}
